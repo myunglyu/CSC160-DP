3f511dddb3eeec043ac02699baba91509314a5f5
myunglyu <180991957+myunglyu@users.noreply.github.com>
2024-09-26 14:38:27 -0700

    modified:   space_tigersapp/__pycache__/urls.cpython-312.pyc 	modified:   space_tigersapp/templates/base.html 	modified:   space_tigersapp/urls.py 	modified:   v-env/Lib/site-packages/django/contrib/staticfiles/__pycache__/storage.cpython-312.pyc 	modified:   v-env/Lib/site-packages/tzdata/__pycache__/__init__.cpython-312.pyc 	modified:   v-env/Lib/site-packages/tzdata/zoneinfo/__pycache__/__init__.cpython-312.pyc

--- space_tigersapp/urls.py
+++ space_tigersapp/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index),
-    path('new', views.new),
+    path('main', views.index),
     path('customers', views.customers)
 ]
