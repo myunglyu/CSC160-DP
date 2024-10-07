2dd93fddaf27edb875192028819f7ab4ace60e50
myunglyu <180991957+myunglyu@users.noreply.github.com>
2024-10-07 09:22:56 -0700

    modified:   db.sqlite3 	modified:   space_tigersapp/__pycache__/admin.cpython-312.pyc 	modified:   space_tigersapp/__pycache__/apps.cpython-312.pyc 	modified:   space_tigersapp/__pycache__/models.cpython-312.pyc 	modified:   space_tigersapp/__pycache__/urls.cpython-312.pyc 	modified:   space_tigersapp/__pycache__/views.cpython-312.pyc 	modified:   space_tigersapp/models.py 	modified:   space_tigersapp/static/styles.css 	modified:   space_tigersapp/templates/base.html 	modified:   space_tigersapp/templates/customers.html 	modified:   space_tigersapp/urls.py 	modified:   space_tigersapp/views.py 	space_tigersapp/static/body-background-image.jpg 	space_tigersapp/templates/customer_delete.html 	space_tigersapp/templates/customer_new.html 	space_tigersapp/templates/customer_update_form.html 	space_tigersapp/templates/login.html

--- space_tigersapp/urls.py
+++ space_tigersapp/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.index),
     path('customers/', views.customers),
-    path('newcustomer/', views.newcustomer),
+    path('customers/new', views.newcustomer.as_view()),
     path('customers/<pk>/update', views.updatecustomer.as_view()),
     path('customers/<pk>/delete', views.deletecustomer.as_view()),
     # path('success/', views.success_view, name='success'),
--- space_tigersapp/views.py
+++ space_tigersapp/views.py
@@ -1,30 +1,25 @@
-from django.shortcuts import render, redirect, get_object_or_404
+from django.shortcuts import render, redirect
 # from django.http import HttpResponse
 from django.contrib.auth.decorators import permission_required, login_required
-from django.views.generic.edit import UpdateView, DeleteView
+from django.views.generic.edit import UpdateView, DeleteView, CreateView
 from .models import product, customer
-from .forms import customerform
 
-product_list = product.objects.all()
-customer_list = customer.objects.all()
 
 def index(request):
+    product_list = product.objects.all()
     return render(request, 'index.html', {'products': product_list})
 
+# @login_required
 def customers(request):
+    customer_list = customer.objects.all()
     return render(request, 'customers.html', {'customers': customer_list})
 
-# @login_required
-# @permission_required
-def newcustomer(request):
-    if request.method == 'POST':
-        form = customerform(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect('/customers')
-    else:
-        form = customerform()
-    return render(request, 'newcustomer.html', {'form': form})
+class newcustomer(CreateView):
+    template_name = "customer_new.html"
+    model = customer
+    fields = ["fname", "mi", "lname", "email", "phone", "image_url"]
+    template_name_suffix = "_new"
+    success_url = "/customers"
 
 class updatecustomer(UpdateView):
     template_name = "customer_update_form.html"
@@ -36,6 +31,5 @@ class updatecustomer(UpdateView):
 class deletecustomer(DeleteView):
     template_name = "customer_delete.html"
     model = customer
-    fields = ["fname", "mi", "lname", "email", "phone", "image_url"]
     template_name_suffix = "delete"
     success_url = "/customers"
