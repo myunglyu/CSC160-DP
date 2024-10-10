253e060e09ff69a40a18eda8a12f6bfaee9a4b2e
myunglyu <180991957+myunglyu@users.noreply.github.com>
2024-10-10 16:59:45 -0700

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   cart/__pycache__/__init__.cpython-312.pyc
    	modified:   cart/__pycache__/admin.cpython-312.pyc
    	modified:   cart/__pycache__/apps.cpython-312.pyc
    	modified:   cart/__pycache__/cart.cpython-312.pyc
    	modified:   cart/__pycache__/context_processors.cpython-312.pyc
    	modified:   cart/__pycache__/models.cpython-312.pyc
    	modified:   cart/__pycache__/urls.cpython-312.pyc
    	modified:   cart/__pycache__/views.cpython-312.pyc
    	modified:   cart/migrations/__pycache__/__init__.cpython-312.pyc
    	modified:   db.sqlite3
    	modified:   space_tigers/__pycache__/__init__.cpython-312.pyc
    	modified:   space_tigers/__pycache__/settings.cpython-312.pyc
    	modified:   space_tigers/__pycache__/urls.cpython-312.pyc
    	modified:   space_tigers/__pycache__/wsgi.cpython-312.pyc
    	modified:   space_tigers/settings.py
    	modified:   space_tigers/urls.py
    	modified:   space_tigersapp/__pycache__/__init__.cpython-312.pyc
    	modified:   space_tigersapp/__pycache__/admin.cpython-312.pyc
    	modified:   space_tigersapp/__pycache__/apps.cpython-312.pyc
    	modified:   space_tigersapp/__pycache__/forms.cpython-312.pyc
    	modified:   space_tigersapp/__pycache__/models.cpython-312.pyc
    	modified:   space_tigersapp/__pycache__/urls.cpython-312.pyc
    	modified:   space_tigersapp/__pycache__/views.cpython-312.pyc
    	modified:   space_tigersapp/forms.py
    	modified:   space_tigersapp/migrations/__pycache__/0001_initial.cpython-312.pyc
    	modified:   space_tigersapp/migrations/__pycache__/0002_alter_customer_phone.cpython-312.pyc
    	modified:   space_tigersapp/migrations/__pycache__/0003_alter_customer_mi.cpython-312.pyc
    	modified:   space_tigersapp/migrations/__pycache__/0004_alter_customer_mi.cpython-312.pyc
    	modified:   space_tigersapp/migrations/__pycache__/0005_alter_customer_email_alter_customer_fname_and_more.cpython-312.pyc
    	modified:   space_tigersapp/migrations/__pycache__/__init__.cpython-312.pyc
    	modified:   space_tigersapp/templates/base.html
    	modified:   space_tigersapp/templates/index.html
    	deleted:    space_tigersapp/templates/login.html
    	modified:   space_tigersapp/urls.py
    	modified:   space_tigersapp/views.py

--- space_tigersapp/urls.py
+++ space_tigersapp/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path('customers/<pk>/update', views.updatecustomer.as_view()),
     path('customers/<pk>/delete', views.deletecustomer.as_view()),
     # path('success/', views.success_view, name='success'),
+    path("signup/", views.SignUpView.as_view(), name="signup"),
     path('cart/', include('cart.urls')),
     path('product/<int:pk>', views.Product, name='product'),
     
